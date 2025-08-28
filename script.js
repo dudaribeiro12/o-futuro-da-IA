624ee109142943cf16c6e420fb4d4222aed7428d
Maria Eduarda <145134263+dudaribeiro12@users.noreply.github.com>
2025-08-28 11:36:19 -0300

    l

--- script.js
+++ script.js
@@ -3,3 +3,40 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+   enunciado: "1.A IA será capaz de criar designs personalizados automaticamente, sem a 
+intervenção de designers humanos."
+
+alternativas: Verdadeiro  Falso
+}
+{
+   enunciado:. "2. A IA poderá gerar código automaticamente para funcionalidades como
+ formulários e integração com redes sociais."
+
+alternativas:  Verdadeiro  Falso
+}
+{
+   enunciado:. "3. A IA ajudará na criação de sites mais acessíveis, 
+ajustando design e conteúdo para usuários com deficiência.
+
+alternativas:  Verdadeiro  Falso
+}
+{
+   enunciado:. "4. A IA será capaz de corrigir erros de código de forma autônoma."
+
+alternativas:  Verdadeiro  Falso
+}
+
+{
+   enunciado:. "5. A IA será capaz de corrigir erros de código de forma autônoma5. A IA poderá personalizar a experiência do usuário, 
+mostrando conteúdo relevante com base em seu comportamento de navegação.."
+
+alternativas:  Verdadeiro  Falso
+}
+{
+   enunciado:. "6.Os sites criados por IA terão a capacidade de adaptar o conteúdo em tempo real para otimizar a experiência do usuário.."
+
+alternativas:  Verdadeiro  Falso
+}
