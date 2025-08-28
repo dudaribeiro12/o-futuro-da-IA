de0524d5f88a926560fa849eae179d6a6aa3736a
Maria Eduarda <145134263+dudaribeiro12@users.noreply.github.com>
2025-08-28 11:41:38 -0300

    kk

--- script.js
+++ script.js
@@ -9,34 +9,34 @@ const perguntas = [
    enunciado: "1.A IA será capaz de criar designs personalizados automaticamente, sem a 
 intervenção de designers humanos."
 
-alternativas: Verdadeiro  Falso
+alternativas: "Verdadeiro,  Falso"
 }
 {
    enunciado:. "2. A IA poderá gerar código automaticamente para funcionalidades como
  formulários e integração com redes sociais."
 
-alternativas:  Verdadeiro  Falso
+alternativas:  "Verdadeiro,  Falso"
 }
 {
    enunciado:. "3. A IA ajudará na criação de sites mais acessíveis, 
 ajustando design e conteúdo para usuários com deficiência.
 
-alternativas:  Verdadeiro  Falso
+alternativas: "Verdadeiro,  Falso"
 }
 {
    enunciado:. "4. A IA será capaz de corrigir erros de código de forma autônoma."
 
-alternativas:  Verdadeiro  Falso
+alternativas:  "Verdadeiro,  Falso"
 }
 
 {
    enunciado:. "5. A IA será capaz de corrigir erros de código de forma autônoma5. A IA poderá personalizar a experiência do usuário, 
 mostrando conteúdo relevante com base em seu comportamento de navegação.."
 
-alternativas:  Verdadeiro  Falso
+alternativas:  "Verdadeiro,  Falso"
 }
 {
    enunciado:. "6.Os sites criados por IA terão a capacidade de adaptar o conteúdo em tempo real para otimizar a experiência do usuário.."
 
-alternativas:  Verdadeiro  Falso
+alternativas:  "Verdadeiro,  Falso"
 }
